cf60be994d849b7b4677ccc002a61340a5f9f80a
Philippe Coval <rzr@gna.org>
2011-07-12 19:22:57 +0200

    build: qt

---
+++ mk-local.mk
@@ -0,0 +1,12 @@
+#! /usr/bin/make -f
+
+generated_files=\
+ src/tools/gencpu \
+#}
+
+distclean:
+	-make $@
+	-make clean
+	find . -type l -exec rm -fv '{}' ;\
+	find . -type f -iname "*.o" -exec rm -fv '{}' \;
+	rm -f ${generated_files}
